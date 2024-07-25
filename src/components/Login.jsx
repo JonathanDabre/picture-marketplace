24ef9dc81ab0c317da5517926e24d77268758c43
JonathanDabre <jonathandabre01@gmail.com>
2024-07-25 17:13:38 +0530

    fixed conflicting package names

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
-import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
-
-const clientId = "154745809431-6e76of8v1kcrldc79i9bsruhrtpqj7lr.apps.googleusercontent.com";
+import { GoogleLogin } from '@react-oauth/google';
 
 const Login = ({ onSuccess, onFailure }) => {
   return (
-    <GoogleOAuthProvider clientId={clientId}>
-      <div id="signInButton">
-        <GoogleLogin
-          onSuccess={(response) => {
-            onSuccess(response);
-          }}
-          onFailure={(error) => {
-            onFailure(error);
-          }}
-        />
-      </div>
-    </GoogleOAuthProvider>
+    <div id="signInButton">
+      <GoogleLogin
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        logo
+      />
+    </div>
   );
 };
 
